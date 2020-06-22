abe11ad031f18363f470143c23e0848c1a1ea2ce
kakabisht <kakabisht07@gmail.com>
2020-06-22 13:01:11 +0530

    first basic project in java script, the Spy test

---
+++ Part9.js
@@ -0,0 +1,14 @@
+var fn = prompt("What is your First Name? ");
+var ln = prompt("What is your Last Name? ");
+var age=prompt("What is your Age? ");
+var cm = prompt("What is your Height in cm's? ");
+var pn = prompt("What is your Pet's Name? ");
+alert("Thank you so much for the information.");
+
+
+if( (fn[0] == ln[0]) && (age>=20 && age <=30) && (pn>=170) && (pn[pn.length -1] =='y' ) ) {
+    console.log("Welcome Comrade! You've passed the Spy Test");
+}
+else {
+    console.log("Nothing here");
+}
